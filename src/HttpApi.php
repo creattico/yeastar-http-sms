aeb59ad5bb8f40c174354764d36af63d24598f08
creattico <info@creattica.it>
2021-05-03 20:48:11 +0200

    Added debug

--- src/HttpApi.php
+++ src/HttpApi.php
@@ -3,6 +3,13 @@ namespace YeastarHttp;
 
 class HttpApi
 {
+	const DEBUG_OFF = 0;
+
+	const DEBUG_CLIENT = 1;
+
+	const DEBUG_FULL = 2;
+
+
 	/**
 	 * Http protocol for calls
 	 * @var string
@@ -30,10 +37,38 @@ class HttpApi
 
 
 	/**
-	 * Passwword
+	 * Password
 	 * @var string
 	 */
 	protected $password = '';
+
+
+	/**
+	 * To: the recipient phone number
+	 * @var string
+	 */
+	protected $to = '';
+
+
+	/**
+	 * Message: the message to be sent
+	 * @var string
+	 */
+	protected $message = '';
+
+
+	/**
+	 * Gateway port: the yeastar port to use. Default 1
+	 * @var string
+	 */
+	protected $gateway_port = 1;
+
+
+	/**
+	 * Debug
+	 * @var int
+	 */
+	protected $debug = 0;
 
 
 	/**
@@ -100,6 +135,45 @@ class HttpApi
 		$this->password = $password;
 	}
 
+
+	/**
+	 * Set the recipient property
+	 * @return void
+	 */
+	public function setTo(string $to)
+	{
+		$this->to = str_replace(' ', '', $to);
+	}
+
+	/**
+	 * Set the message property, will be urlencoded
+	 * @return void
+	 */
+	public function setMessage(string $message)
+	{
+		$this->message = urlencode($message);
+	}
+
+
+	/**
+	 * Set the recipient property
+	 * @return void
+	 */
+	public function setGatewayPort(int $gateway_port)
+	{
+		$this->gateway_port = $gateway_port;
+	}
+
+
+	/**
+	 * Set the password property
+	 * @return void
+	 */
+	public function setDebugMode(int $debug)
+	{
+		$this->debug = $debug;
+	}
+
 	
 	/**
 	 * Get the full URL for the curl request
@@ -115,11 +189,9 @@ class HttpApi
 	 * 
 	 * @return array A full rapresentative response of the curl request
 	 */
-	public function sendSms($to, $message, $gateway_port = 1)
+	public function sendSms()
 	{
-    $message = urlencode($message);
-
-    $url = $this->getUrl() . "&port=$gateway_port&destination=$to&content=$message";
+    $url = $this->getUrl() . "&port=$this->gateway_port&destination=$this->to&content=$this->message";
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
@@ -142,5 +214,34 @@ class HttpApi
 
 		curl_close($curl);
 		return $result;
+	}
+
+
+	/**
+	 * Send method
+	 * 
+	 * @return mixed
+	 */
+	public function send()
+	{
+		$res = $this->sendSms();
+
+		switch ($this->debug) {
+			case 1:
+				return $res['response'];
+				break;
+
+			case 2:
+				return $res;
+				break;
+			
+			default:
+				if (strpos($res['response'], 'Success')) {
+					return true;
+				} else {
+					return false;
+				}
+				break;
+		}
 	}
 }
